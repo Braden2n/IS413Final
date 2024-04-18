b53b39aa64e96ac02d778ec99c1533c0eccb0ce6
Braden Toone <braden@toonetown.com>
2024-04-17 21:31:49 -0600

    Restructured

--- Models/Entertainer.cs
+++ Models/Entertainer.cs
@@ -1,32 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Final.Models;
 
-public partial class Entertainer
+public class Entertainer
 {
     [Key]
     [Required]
     public int EntertainerId { get; set; }
-
     public string? EntStageName { get; set; }
-
     public string? EntSsn { get; set; }
-
     public string? EntStreetAddress { get; set; }
-
     public string? EntCity { get; set; }
-
     public string? EntState { get; set; }
-
     public string? EntZipCode { get; set; }
-
     public string? EntPhoneNumber { get; set; }
-
     public string? EntWebPage { get; set; }
-
     public string? EntEmailAddress { get; set; }
-
     public DateOnly? DateEntered { get; set; }
 }
