59d52e846e7995b05c7d7eeaaa2509c5ce4d5362
Braden Toone <braden@toonetown.com>
2024-04-17 21:31:40 -0600

    Added necessary signatures

--- Models/EfRepo.cs
+++ Models/EfRepo.cs
@@ -14,4 +14,27 @@ public class EfRepo : IRepo
     {
         return _context.Entertainers.FirstOrDefault(e => e.EntertainerId == id);
     }
+
+    public Entertainer NewEntertainer()
+    {
+        var maxId = _context.Entertainers.Max(e => (int?)e.EntertainerId) ?? 0;
+        var nextId = maxId + 1;
+        var entertainer = new Entertainer
+        {
+            EntertainerId = nextId
+        };
+        return entertainer;
+    }
+
+    public void UpdateEntertainer(Entertainer entertainer)
+    {
+        _context.Entertainers.Update(entertainer);
+        _context.SaveChanges();
+    }
+
+    public void DeleteEntertainer(Entertainer entertainer)
+    {
+        _context.Entertainers.Remove(entertainer);
+        _context.SaveChanges();
+    }
 }
--- Models/IRepo.cs
+++ Models/IRepo.cs
@@ -4,4 +4,7 @@ public interface IRepo
 {
     IQueryable<Entertainer> Entertainers { get; }
     public Entertainer? EntertainerById(int id);
+    public Entertainer NewEntertainer();
+    public void UpdateEntertainer(Entertainer entertainer);
+    public void DeleteEntertainer(Entertainer entertainer);
 }
