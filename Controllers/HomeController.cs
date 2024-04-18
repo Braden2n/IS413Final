c75e014def9535c3774a94b016cf9732e9b5115d
Braden Toone <braden@toonetown.com>
2024-04-17 20:59:31 -0600

    Restructure Controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,15 +6,34 @@ namespace Final.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly IRepo _repo;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(IRepo repo)
     {
-        _logger = logger;
+        _repo = repo;
     }
 
+    [HttpGet]
     public IActionResult Index()
     {
         return View();
+    }
+
+    [HttpGet]
+    public IActionResult Entertainers()
+    {
+        var model = _repo.Entertainers;
+        return View(model);
+    }
+
+    [HttpGet]
+    public IActionResult EntertainerDetail(int id)
+    {
+        var model = _repo.EntertainerById(id);
+        if (model == null)
+        {
+            return RedirectToAction("Entertainers");
+        }
+        return View(model);
     }
 }
