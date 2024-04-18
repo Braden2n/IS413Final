57993be3765bc5409ec2d94d07dca37479393927
Braden Toone <braden@toonetown.com>
2024-04-17 20:27:25 -0600

    Added DbContext to the program.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
+using Final.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("EntertainmentAgency"));
+});
 
 var app = builder.Build();
 
